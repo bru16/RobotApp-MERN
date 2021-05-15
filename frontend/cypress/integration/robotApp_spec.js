e8de91f432aeea88f52254513134f003c5ef7d5e
Bruno <bru_16@hotmail.com.ar>
2021-05-15 17:00:31 -0300

    slight changes

--- frontend/cypress/integration/robotApp_spec.js
+++ frontend/cypress/integration/robotApp_spec.js
@@ -37,18 +37,28 @@ describe('Robot App', () => {
     });
 })
 
-describe('when logged in', () => {
+describe('when logged in (as admin)', () => {
     beforeEach(() => {
-        cy.login({ email: 'admin@admin.com', password: 'admin' })
+        cy.login({ email: 'admin@admin.com', password: 'admin' });
+        cy.createNewRobot({ name: 'Test Robot 1', description: 'Test description 1', url: 'https://www.youtube.com/watch?v=zPyg4N7bcHM' });
+        cy.createNewRobot({ name: 'Test Robot 2', description: 'Test description 2', url: 'https://www.youtube.com/watch?v=zPyg4N7bcHM' });
+        cy.createNewRobot({ name: 'Wow!', description: 'Nice!', url: 'https://www.youtube.com/watch?v=zPyg4N7bcHM' });
     });
 
-    it('can go to favorite robots when having none', () => {
+    it('user sees three robots on home page', () => {
+        cy.contains('Test Robot 1');
+        cy.contains('Test Robot 2');
+        cy.contains('Wow!');
+        cy.contains('Nice!');
+    })
+
+    it('user can go to favorite robots when having none', () => {
         cy.contains('My Favorites');
         cy.get('[data-test-id="favoriteRobotsUser"]').click();
         cy.contains('No favorites Robots found, try adding one!');
     });
 
-    it('can create a new Robot correctly', () => {
+    it('user can create a new Robot correctly', () => {
         cy.contains('Add a robot!').click();
         cy.get('input').first().type('Hello');
         cy.get('textarea').type('This is a test from cypress :)');
@@ -62,7 +72,7 @@ describe('when logged in', () => {
         cy.contains('This is a test from cypress :)');
     });
 
-    it('add robot to favorite and checks if is it faved', () => {
+    it('user add robot to favorite and checks if is it faved', () => {
         cy.contains('More Info').click();
         cy.contains('Hello');
         cy.contains('This is a test from cypress :)');
--- frontend/src/Components/auth/SignupForm.js
+++ frontend/src/Components/auth/SignupForm.js
@@ -46,51 +46,49 @@ const SignupForm = () => {
                 <Link to="/"><button type="button" className="btn btn-dark" ><FontAwesomeIcon icon={faReply} /> Go back</button></Link>
             </div>
             <div className="container mt-5">
-                <div className="row ">
-                    <div className="col-md-3 mx-auto text-center">
-                        <Formik initialValues={{
-                            username: '',
-                            password: '',
-                            email: '',
-                            confirmPassword: '',
-                            role: 'user'
-                        }} onSubmit={values => handleSubmit(values)}
-                            validationSchema={SignupSchema}>
+                <div className="col-md-4 mx-auto">
+                    <Formik initialValues={{
+                        username: '',
+                        password: '',
+                        email: '',
+                        confirmPassword: '',
+                        role: 'user'
+                    }} onSubmit={values => handleSubmit(values)}
+                        validationSchema={SignupSchema}>
 
-                            {({ errors, touched }) => (
-                                <Form>
-                                    <h3>Email</h3>
-                                    <Field name="email" />
-                                    {errors.email && touched.email ? <div className="formError">{errors.email}</div> : null}
+                        {({ errors, touched }) => (
+                            <Form className="text-center">
+                                <h3>Email</h3>
+                                <Field className="form-input" name="email" />
+                                {errors.email && touched.email ? <div className="formError">{errors.email}</div> : null}
 
-                                    <h3>Username</h3>
-                                    <Field name="username" />
-                                    {errors.username && touched.username ? (
-                                        <div className="formError">{errors.username}</div>
-                                    ) : null}
+                                <h3>Username</h3>
+                                <Field className="form-input" name="username" />
+                                {errors.username && touched.username ? (
+                                    <div className="formError">{errors.username}</div>
+                                ) : null}
 
-                                    <h3>Password</h3>
-                                    <Field name="password" type="password" />
-                                    {errors.password && touched.password ? <div className="formError">{errors.password}</div> : null}
+                                <h3>Password</h3>
+                                <Field className="form-input" name="password" type="password" />
+                                {errors.password && touched.password ? <div className="formError">{errors.password}</div> : null}
 
-                                    <h3>Confirm password</h3>
-                                    <Field name="confirmPassword" type="password" />
-                                    {errors.confirmPassword && touched.confirmPassword ? <div className="formError">{errors.confirmPassword}</div> : null}
+                                <h3>Confirm password</h3>
+                                <Field className="form-input" name="confirmPassword" type="password" />
+                                {errors.confirmPassword && touched.confirmPassword ? <div className="formError">{errors.confirmPassword}</div> : null}
 
-                                    <div className="mt-3">
-                                        <Field name="role" component="select">
-                                            <option value='user'>user</option>
-                                            <option value="moderator">moderator</option>
-                                            <option value="admin">admin</option>
-                                        </Field>
-                                    </div>
-                                    <div className="mt-4">
-                                        <button type="submit" className="btn btn btn-dark btn-space" >Submit</button>
-                                    </div>
-                                </Form>
-                            )}
-                        </Formik>
-                    </div>
+                                <div className="mt-3">
+                                    <Field name="role" component="select">
+                                        <option value='user'>user</option>
+                                        <option value="moderator">moderator</option>
+                                        <option value="admin">admin</option>
+                                    </Field>
+                                </div>
+                                <div className="mt-4">
+                                    <button type="submit" className="btn btn btn-dark btn-space" >Submit</button>
+                                </div>
+                            </Form>
+                        )}
+                    </Formik>
                 </div>
 
             </div>
